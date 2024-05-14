cdf006de350666fc60823340ab8ec7f74ca5801f
Mert Aksoy <88950132+Fencer1919@users.noreply.github.com>
2024-05-14 16:55:34 +0300

    Update check_map.c

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maksoy <maksoy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maksoy <42istanbul.com.tr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 01:38:01 by maksoy            #+#    #+#             */
 /*   Updated: 2022/10/13 01:50:34 by maksoy           ###   ########.fr       */
